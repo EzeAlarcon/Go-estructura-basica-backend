3d8fbad1b8e69a54bda4793a11d032e971481adf
Ezequiel Alarcon <138638611+EzeAlarcon@users.noreply.github.com>
2024-10-20 21:32:56 -0500

    Update handler.customer.go

--- services/customer/delivery/http/handler.customer.go
+++ services/customer/delivery/http/handler.customer.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/oniharnantyo/golang-backend-example/domain"
+	"github.com/EzeAlarcon/golang-backend-example/domain"
 
 	"github.com/pkg/errors"
 
